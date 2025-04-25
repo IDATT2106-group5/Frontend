069e8831fe182a3fdfe51162a0f7465a06199de8
Nathalie Graidy <nathalga@ntnu.no>
2025-04-25 13:05:12 +0200

    refactor: Added storageNavbar to storageView

--- src/views/StorageView.vue
+++ src/views/StorageView.vue
@@ -1,15 +1,29 @@
 <script>
+import { defineComponent } from 'vue'
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import Navbar from '@/components/Navbar.vue'
+import StorageNavbar from '@/components/StorageNavbar.vue'
+
+
+export default defineComponent({
+  components: { StorageNavbar, Accordion, AccordionContent, AccordionItem, AccordionTrigger, Navbar }
+})
 
 
 </script>
 
 <template>
+  <div>
+    <Navbar />
+    <StorageNavbar />
+  </div>
+
+
   <Accordion type="single" collapsible>
     <AccordionItem value="item-1">
       <AccordionTrigger>Is it accessible?</AccordionTrigger>
