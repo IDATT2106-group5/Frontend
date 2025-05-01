06326356b50d5012601a0b2b138aa4471863ea6e
Jon Bergland <jobergl@stud.ntnu.no>
2025-05-01 14:06:39 +0200

    feat: add method to send verify2FA-post request in TwoFactorAuthService

--- src/service/adminService.js
+++ src/service/adminService.js
@@ -1,6 +1,13 @@
 import apiClient from '@/service/apiClient';
 
-class AdminService {
+/**
+ * Service for handling Two-Factor Authentication (2FA) operations for admin users.
+ *
+ * This service provides methods to generate and verify 2FA codes for enhanced security during the login process.
+ *
+ * @class TwoFactorAuthService
+ */
+class TwoFactorAuthService {
 
   /**
    * Generates a Two-Factor Authentication (2FA) code for the given email.
@@ -14,6 +21,18 @@ class AdminService {
     return apiClient.post("/admin/login/2fa/generate", { email });
   }
 
+  /**
+   * Verifies the two-factor authentication (2FA) credentials for an admin user.
+   *
+   * @param {Object} credentials       - The 2FA credentials to verify.
+   * @param {string} credentials.email - The users email that the 2FA code has been sent to.
+   * @param {string} credentials.otp   - The 6 digit code that the user typed in.
+   * @returns {Promise<Object>} A promise that resolves to the server's response.
+   */
+  async verify2FA(credentials) {
+    return apiClient.post("/admin/login/2fa/verify", credentials);
+  }
+
 }
 
-export default new AdminService();
+export default new TwoFactorAuthService();
