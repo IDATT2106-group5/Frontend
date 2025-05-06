799af32a850f122f88a3a326328746554adee20d
Jon Bergland <jobergl@stud.ntnu.no>
2025-05-06 14:57:24 +0200

    feat: initialize AdminUserView with Admin components

---
+++ src/views/adminViews/AdminUserView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import InviteNewAdmin from "@/components/adminComponents/InviteNewAdmin.vue";
+import AdminUserOverview from "@/components/adminComponents/AdminUserOverview.vue";
+</script>
+
+<template>
+  <InviteNewAdmin
+  />
+  <AdminUserOverview
+  />
+
+</template>
