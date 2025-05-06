938aec70f5a223ce9971e0ba0ec491474be4edaa
Jon Bergland <jobergl@stud.ntnu.no>
2025-05-06 15:39:28 +0200

    refactor: add styling to AdminUsersView for improved layout

--- src/views/adminViews/AdminUsersView.vue
+++ src/views/adminViews/AdminUsersView.vue
@@ -1,12 +1,21 @@
 <script setup>
 import InviteNewAdmin from "@/components/adminComponents/InviteNewAdmin.vue";
 import AdminUserOverview from "@/components/adminComponents/AdminUsersOverview.vue";
+
+function handleInvite(adminData) {
+  console.log('New admin invitation:', adminData);
+  // TODO: Add API logic here
+}
 </script>
 
 <template>
-  <InviteNewAdmin
-  />
-  <AdminUserOverview
-  />
+    <div class="min-h-screen p-6 bg-gray-100">
+      <div class="max-w-4xl mx-auto space-y-8">
+        <div class="flex justify-center">
+          <InviteNewAdmin @invite-admin="handleInvite" />
+        </div>
 
+        <AdminUserOverview />
+      </div>
+    </div>
 </template>
