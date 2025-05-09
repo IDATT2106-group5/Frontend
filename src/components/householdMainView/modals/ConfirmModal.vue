146f4ea3a71641d10f27268551cef459ecaea987
Nathalie Graidy <nathalga@ntnu.no>
2025-05-09 20:23:17 +0200

    feat: add data-cy attributes for testing and refactor storage detail view tests

--- src/components/householdMainView/modals/ConfirmModal.vue
+++ src/components/householdMainView/modals/ConfirmModal.vue
@@ -54,6 +54,7 @@ onBeforeUnmount(() => {
             ref="confirmButtonRef"
             @click="emit('confirm')"
             class="px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-700"
+            data-cy="modal-confirm-button"
           >
             {{ confirmText }}
           </button>
