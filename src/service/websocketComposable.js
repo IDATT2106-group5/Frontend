8f36d48863767b9d12070fa3817eea8aaa15408e
Tam Le <tamle2107@hotmail.com>
2025-05-09 08:21:44 +0200

    feat: added tests for websockets

--- src/service/websocketComposable.js
+++ src/service/websocketComposable.js
@@ -161,6 +161,7 @@ export default function useWebSocket() {
     resetNotificationCount,
     subscribeToPosition,
     updatePosition,
+    fetchNotifications,
     fetchHouseholdPositions,
     showIncidentPopup,
     currentIncident,
